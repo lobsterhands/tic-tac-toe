fc084fe024dcbfcb50fd4411b7daf62016d1c360
Lyle Denman <denmanlp@gmail.com>
2017-03-27 14:06:42 -0500

    Convert from tab to spaces

--- src/tic-tac-toe/board.js
+++ src/tic-tac-toe/board.js
@@ -3,33 +3,33 @@ import Square from './square.js';
 
 class Board extends Component {
 
-	renderSquare(i) {
-		return (
-			<Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>
-		);
-	}
+    renderSquare(i) {
+        return (
+            <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>
+        );
+    }
 
-	render() {
-		return (
-			<div className="Board">
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="Board">
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Board;
--- src/tic-tac-toe/game.js
+++ src/tic-tac-toe/game.js
@@ -4,49 +4,49 @@ import './game.css';
 
 class Game extends Component {
 
-	constructor() {
-		super();
-		this.state = {
-			history: [
-				{
-					squares: Array(9).fill(null)
-				}
-			],
-			xIsNext: true,
-		}
-	}
+    constructor() {
+        super();
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null)
+                }
+            ],
+            xIsNext: true,
+        }
+    }
 
-	handleClick(i) {
-		const history = this.state.history;
-		const current = history[history.length - 1];
-		const squares = current.squares.slice();
-		if (squares[i]) {
-			return;
-		}
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (squares[i]) {
+            return;
+        }
 
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
 
-		this.setState({
-			history: history.concat([{
-				squares: squares
-			}]),
-			xIsNext: !this.state.xIsNext,
-		});
-	}
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
+    }
 
-	render() {
-		const history = this.state.history;
-		const current = history[history.length - 1];
-		const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
-		return (
-			<div className="Game">
-				<div className="status">
-					{status}
-				</div>
-				<Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
-			</div>
-		);
-	}
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+        return (
+            <div className="Game">
+                <div className="status">
+                    {status}
+                </div>
+                <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
+            </div>
+        );
+    }
 }
 
 export default Game;

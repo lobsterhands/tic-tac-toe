174662c7f295df0f64b1e40789b046b2a50f7ef5
Lyle Denman <denmanlp@gmail.com>
2017-03-28 13:06:46 -0500

    Update className based on highlight condition

--- src/tic-tac-toe/square.jsx
+++ src/tic-tac-toe/square.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+
     render() {
+        const highlight = (this.props.highlight ? "highlight" : "");
+
         return(
-            <button className="square" onClick={() => this.props.onClick()}>
+            <button className={"square " + highlight} onClick={() => this.props.onClick()}>
                 {this.props.value}
             </button>
         );
